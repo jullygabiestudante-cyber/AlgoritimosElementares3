47ed93c926acf71807254f18c916eae750ca0b73
Julia <jullygabiestudante@gmail.com>
2026-09-03 18:52:26 -0300

    Acrescentando Algoritimos 4 e 5

--- src/ed-2-Exercicio4-BubleSort.cpp
+++ src/ed-2-Exercicio4-BubleSort.cpp
@@ -1,1 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
 
+int main() {
+    int n;
+    cout << "Informe a quantidade de Elementos" << endl;
+    cin >> n;
+    int num[n];
+
+    int aux;
+    for (int i = 0; i < n; i++) {
+        cin >> num[i];
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n - 1- i; j++) {
+            if (num[j] % 2 != 0 && num[j + 1] % 2 == 0) {
+                aux = num[j];
+                num[j] = num[j + 1];
+                num[j + 1] = aux;
+            } else if (num[j] % 2 == 0 && num[j + 1] % 2 == 0 && num[j] > num[j + 1]) {
+                aux = num[j];
+                num[j] = num[j + 1];
+                num[j + 1] = aux;
+            } else if (num[j] % 2 != 0 && num[j + 1] % 2 != 0 && num[j] < num[j + 1]) {
+                aux = num[j];
+                num[j] = num[j + 1];
+                num[j + 1] = aux;
+            }
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        cout << num[i] << " ";
+    }
+}
--- src/ed-2-Exercicio5-InsertSort.cpp
+++ src/ed-2-Exercicio5-InsertSort.cpp
@@ -1,3 +1,23 @@
-//
-// Created by Julia on 27/08/2026.
-//
+#include <iostream>
+using namespace std;
+int main() {
+    int n, cont = 0, aux,j;
+    cout<<"Informe a quantidade de elementos :"<<endl;
+    cin>>n;
+    int num[n];
+    for(int i=0;i<n;i++) cin>>num[i];
+
+    for(int i=1;i<n;i++) {
+        aux = num[i];
+        j = i -1;
+        while ( j >=  0 &&  num[j] >  aux) {
+            num[j + 1] = num[j];
+            j--;
+            cont ++;
+        }
+        num[j+1] = aux;
+
+    }
+
+    cout<< cont <<endl;
+}
